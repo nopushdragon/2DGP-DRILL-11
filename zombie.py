bd8ad7123481b9b624232bcf16fd1e43af9127ec
AN MIN YONG <iks05280204@gmail.com>
2025-11-06 19:20:30 +0900

    날라가는 공에 맞으면 좀비 크기가 줄긴 하는데 중앙 기준이라 한번 맞으면 작아져서 공중에 뜬다

--- zombie.py
+++ zombie.py
@@ -33,10 +33,13 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.hp = 2
+        self.size_x = 200
+        self.size_y = 200
 
 
     def get_bb(self):
-        return self.x - 100, self.y - 100, self.x + 100, self.y + 100
+        return self.x - self.size_x/2, self.y - self.size_y/2, self.x + self.size_x/2, self.y + self.size_y/2
 
     def update(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
@@ -50,9 +53,9 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.size_x, self.size_y)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.size_x, self.size_y)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
@@ -60,4 +63,11 @@ class Zombie:
 
     def handle_collision(self, group, other):
         if group == 'zombie:ball':
-            game_world.remove_object(self)
+            if other.stopped == False:
+                if self.hp == 2:
+                    self.hp -= 1
+                    self.size_y /= 2
+                    self.size_x /= 2
+                elif self.hp == 1:
+                    game_world.remove_object(self)
+
